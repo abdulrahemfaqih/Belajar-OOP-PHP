dd4345b6a92dc3423264d8fecbb312851ae2b65b
abdulrahemfaqih <faqih3935@gmail.com>
2023-07-06 12:01:31 +0700

    memperbaiki inheritance

--- 4.inheritance/inheritance.php
+++ 4.inheritance/inheritance.php
@@ -8,10 +8,8 @@ class Produk
   public $harga;
   public $jmlHalaman;
   public $waktuMain;
-  public $tipe;
 
-
-  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0, $tipe)
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0)
   {
     $this->judul = $judul;
     $this->penulis = $penulis;
@@ -19,18 +17,12 @@ class Produk
     $this->harga = $harga;
     $this->jmlHalaman = $jmlHalaman;
     $this->waktuMain = $waktuMain;
-    $this->tipe = $tipe;
   }
 
   public function getLabel()
   {
     return "{$this->penulis}, {$this->penerbit}";
   }
-  // public function getInfoProduk()
-  // {
-  //   $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
-  //   return $str;
-  // }
 }
 
 
@@ -65,8 +57,8 @@ class CetakInfoProduct
 
 
 
-$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 10000, 100, 0, "Komik");
-$produk2 = new Game("Free Fire", "Abdul Rahem Faqih", "Garena", 0, 0, 0, "Game");
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 10000, 1000);
+$produk2 = new Game("Free Fire", "Abdul Rahem Faqih", "Garena", 10000,0, 200000);
 
 echo "Komik: " . $produk1->getLabel() . "<br>";
 echo "Game: " . $produk2->getLabel() . "<br>";
