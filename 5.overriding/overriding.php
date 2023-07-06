d9dc2f440145de54375a1ccef7c7213f0b076ee4
abdulrahemfaqih <faqih3935@gmail.com>
2023-07-06 13:05:44 +0700

    mengupdate materi overriding

--- 5.overriding/overriding.php
+++ 5.overriding/overriding.php
@@ -2,69 +2,81 @@
 
 class Produk
 {
-  public $judul;
-  public $penulis;
-  public $penerbit;
-  public $harga;
-  public $jmlHalaman;
-  public $waktuMain;
+   public $judul;
+   public $penulis;
+   public $penerbit;
+   public $harga;
 
-  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0)
-  {
-    $this->judul = $judul;
-    $this->penulis = $penulis;
-    $this->penerbit = $penerbit;
-    $this->harga = $harga;
-    $this->jmlHalaman = $jmlHalaman;
-    $this->waktuMain = $waktuMain;
-  }
+   public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
+   {
+      $this->judul = $judul;
+      $this->penulis = $penulis;
+      $this->penerbit = $penerbit;
+      $this->harga = $harga;
+   }
 
-  public function getLabel()
-  {
-    return "{$this->penulis}, {$this->penerbit}";
-  }
+   public function getLabel()
+   {
+      return "{$this->penulis}, {$this->penerbit}";
+   }
 
-  public function getInfoProduk()
-  {
-    $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
-    return $str;
-  }
+   public function getInfoProduk()
+   {
+      $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+      return $str;
+   }
 }
 
 
 class Komik extends Produk
 {
-  public function getInfoProduk()
-  {
-    $str = "Komik :" . parent::getInfoProduk() . " - {$this->jmlHalaman} Halaman";
-    return $str;
-  }
+   public $jmlHalaman;
+
+   public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0)
+   {
+      parent::__construct($judul, $penulis, $penerbit, $harga);
+      $this->jmlHalaman = $jmlHalaman;
+   }
+
+   public function getInfoProduk()
+   {
+      $str = "Komik :" . parent::getInfoProduk() . " - {$this->jmlHalaman} Halaman";
+      return $str;
+   }
 }
 
 class Game extends Produk
 {
-  public function getInfoProduk()
-  {
-    $str = "Game :" . parent::getInfoProduk() . "- {$this->waktuMain} Jam";
-    return $str;
-  }
+   public $waktuMain;
+
+   public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $waktuMain = 0)
+   {
+      parent::__construct($judul, $penulis, $penerbit, $harga);
+      $this->waktuMain = $waktuMain;
+   }
+
+   public function getInfoProduk()
+   {
+      $str = "Game :" . parent::getInfoProduk() . "- {$this->waktuMain} Jam";
+      return $str;
+   }
 }
 
 
 
 class CetakInfoProduct
 {
-  public function cetak(Produk $produk)
-  {
-    $str = "<br>{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
-    return $str;
-  }
+   public function cetak(Produk $produk)
+   {
+      $str = "<br>{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+      return $str;
+   }
 }
 
 
 
 $produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 10000, 1000);
-$produk2 = new Game("Free Fire", "Abdul Rahem Faqih", "Garena", 10000, 0, 200000);
+$produk2 = new Game("Free Fire", "Abdul Rahem Faqih", "Garena", 10000, 200000);
 
 echo "Komik: " . $produk1->getLabel() . "<br>";
 echo "Game: " . $produk2->getLabel() . "<br>";
